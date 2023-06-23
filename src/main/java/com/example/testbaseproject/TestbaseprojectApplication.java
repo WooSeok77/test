178f660dc32801e191ac29f6167a2a1109e6be04
WooSeok77 <wi8tkf@naver.com>
2023-06-23 15:10:56 +0900

    test

---
+++ src/main/java/com/example/testbaseproject/TestbaseprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.testbaseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestbaseprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestbaseprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testbaseproject/TestbaseprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testbaseproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestbaseprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
